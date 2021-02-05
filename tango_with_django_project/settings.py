dbf3fce52a7fa87c495535b706130a8ad7d71a70
Yash Srivastava <2572266s@student.gla.ac.uk>
2021-02-05 14:52:53 +0530

    chapter 4 final commit

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -11,11 +11,13 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+#------------------------------------------------------------------------------------------
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+#------------------------------------------------------------------------------------------
 
 
 
@@ -32,7 +34,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+#------------------------------------------------------------------------------------------
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -42,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rango',
 ]
-
+#------------------------------------------------------------------------------------------
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -52,9 +54,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+#------------------------------------------------------------------------------------------
 ROOT_URLCONF = 'tango_with_django_project.urls'
-
+#------------------------------------------------------------------------------------------
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -66,17 +68,19 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
             ],
         },
     },
 ]
-
+#------------------------------------------------------------------------------------------
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
+#------------------------------------------------------------------------------------------
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -84,7 +88,7 @@ DATABASES = {
     }
 }
 
-
+#------------------------------------------------------------------------------------------
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
@@ -102,11 +106,11 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+#------------------------------------------------------------------------------------------
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
-
+#------------------------------------------------------------------------------------------
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -116,11 +120,17 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
+#------------------------------------------------------------------------------------------
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+#------------------------------------------------------------------------------------------
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR, ]
+#------------------------------------------------------------------------------------------
 
+
+# Media files
+#------------------------------------------------------------------------------------------
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
